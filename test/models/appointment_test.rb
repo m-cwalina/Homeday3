f9e68608b4bababae859bdcf1e375c570f433f66
Matthew Cwalina <matthewcwalina@gmail.com>
2022-06-03 10:32:52 +0200

    Created tests from my validations in models

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -1,5 +1,18 @@
 require "test_helper"
 
 class AppointmentTest < ActiveSupport::TestCase
+  test "should not create appointment without address" do
+    seller = Seller.new
+    assert_not seller.save, "Saved the appointment without a address"
+  end
 
+  test "should not create appointment without a timeslot" do
+    seller = Seller.new
+    assert_not seller.save, "Saved the appointment without a timeslot"
+  end
+
+  test "should not create appointment without a date" do
+    seller = Seller.new
+    assert_not seller.save, "Saved the appointment without a date"
+  end
 end
--- test/models/seller_test.rb
+++ test/models/seller_test.rb
@@ -3,6 +3,11 @@ require "test_helper"
 class SellerTest < ActiveSupport::TestCase
   test "should not save seller without name" do
     seller = Seller.new
-    assert_not seller.save, "Saved the article without a title"
+    assert_not seller.save, "Saved the seller without a name"
+  end
+
+  test "should not save seller without a phone number" do
+    seller = Seller.new
+    assert_not seller.save, "Saved the seller without a phone number"
   end
 end
